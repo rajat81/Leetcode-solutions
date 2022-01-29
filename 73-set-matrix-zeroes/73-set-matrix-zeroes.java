3d4c23c0147ca175e8597eb50faa9400c845e656
Rajat sawant <rajatswnt@gmail.com>
2022-01-29 20:08:20 +0530

    Time: 1 ms (95.88%), Space: 54 MB (6.27%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -3,24 +3,31 @@ class Solution {
         int H = mat.length;
         int W = mat[0].length;
         
-        boolean[] isZero = new boolean[W];
+   //     boolean[] isZero = new boolean[W];
+        boolean isFirstZero = false;
         
         for(int row = 0; row<H; ++row) {
             for(int col = 0; col<W; ++col) {
+                if(row == 0 && mat[row][col] == 0) isFirstZero = true;
                 if (mat[row][col] == 0) 
-                    isZero[col] = true;
+                    mat[0][col] = 0;    // whole col should be zero
             }
         }
         
         
-        for(int row = 0; row<H; row++) {
+        for(int row = 1; row<H; row++) {
             boolean zero = false;
             for(int col = 0; col<W; col++) {
                 if(mat[row][col] == 0) zero = true;    
             }
             
             for(int col = 0; col < W; col++) {
-                if(zero || isZero[col]) mat[row][col] = 0;
+                if(zero || mat[0][col] == 0) mat[row][col] = 0;
+            }
+        }
+        if(isFirstZero) {
+            for(int col = 0; col<W; col++) {
+                mat[0][col] = 0;    
             }
         }
     }
