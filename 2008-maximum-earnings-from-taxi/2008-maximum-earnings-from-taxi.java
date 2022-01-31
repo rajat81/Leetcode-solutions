f044bb3797c3544f71932e46c2a323267b806aad
Rajat sawant <rajatswnt@gmail.com>
2022-02-01 00:39:58 +0530

    Time: 137 ms (35.10%), Space: 131.4 MB (19.45%) - LeetHub

--- 2008-maximum-earnings-from-taxi/2008-maximum-earnings-from-taxi.java
+++ 2008-maximum-earnings-from-taxi/2008-maximum-earnings-from-taxi.java
@@ -2,8 +2,8 @@ class Solution {
     public long maxTaxiEarnings(int n, int[][] rides) {
         Arrays.sort(rides, (a,b) -> a[1]-b[1]);
         // sort using the end times
-        TreeMap<Integer, Long> profit = new TreeMap<>(); 
-        profit.put(0, 0L);
+        TreeMap<Integer, Long> profit = new TreeMap<>(); // red black tree
+        profit.put(0, 0L);  // To avoid NPE while getting value floor to the p[0] in loop 
         
         for(int[] p: rides) {
             long current = p[1]-p[0]+p[2] + profit.floorEntry(p[0]).getValue();
